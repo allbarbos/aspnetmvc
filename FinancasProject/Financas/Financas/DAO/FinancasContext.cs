5a1d4b82a5bef87bd955c78179beec25bf38e622
Allan Barbosa <allan.barbosa@cenariocapital.com.br>
2017-01-09 11:42:27 -0200

    Finanças: Aula 1 - Configuração do Projeto/Entity e implementação de Migrations

---
+++ FinancasProject/Financas/Financas/DAO/FinancasContext.cs
@@ -0,0 +1,23 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class FinancasContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Movimentacao> Movimentacoes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Movimentação tem FK de Usuario, sendo assim, toda movimentação precisa ter um usuário
+            modelBuilder.Entity<Movimentacao>().HasRequired(mov => mov.Usuario);
+        }
+
+    }
+}
