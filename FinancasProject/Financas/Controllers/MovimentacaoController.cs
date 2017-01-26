6f9aabe60955a00b2a0ca2533c567a7592788cff
Allan Honorato Barbosa <allanbarbosa@cenariocapital.ddns.net>
2017-01-26 13:11:29 -0200

    Finanças: Aula 4 - Cadastro de movimentações

---
+++ FinancasProject/Financas/Controllers/MovimentacaoController.cs
@@ -0,0 +1,46 @@
+﻿using Financas.DAO;
+using Financas.Entidades;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Financas.Controllers
+{
+    public class MovimentacaoController : Controller
+    {
+        private MovimentacaoDAO movDAO;
+        private UsuarioDAO userDAO;
+
+        public MovimentacaoController(MovimentacaoDAO movimentacaoDAO, UsuarioDAO usuarioDAO)
+        {
+            movDAO = movimentacaoDAO;
+            userDAO = usuarioDAO;
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Usuarios = userDAO.Lista();
+            return View();
+        }
+
+        public ActionResult Adiciona(Movimentacao movimentacao)
+        {
+            if (ModelState.IsValid)
+            {
+                movDAO.Adiciona(movimentacao);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Usuarios = userDAO.Lista();
+                return View("Form", movimentacao);
+            }
+        }
+
+        public ActionResult Index()
+        {
+            IList<Movimentacao> movimentacoes = movDAO.Lista();
+            return View(movimentacoes);
+        }
+    }
+}
---
+++ FinancasProject/Financas/DAO/MovimentacaoDAO.cs
@@ -0,0 +1,29 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class MovimentacaoDAO
+    {
+        private FinancasContext ctx;
+
+        public MovimentacaoDAO(FinancasContext context)
+        {
+            ctx = context;
+        }
+
+        public void Adiciona(Movimentacao movimentacao)
+        {
+            ctx.Movimentacoes.Add(movimentacao);
+            ctx.SaveChanges();
+        }
+
+        public IList<Movimentacao> Lista()
+        {
+            return ctx.Movimentacoes.ToList();
+        }
+    }
+}
