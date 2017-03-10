592b65cb53f02895916fe4373708a25a3dc6d350
Allan Barbosa <allan.hb@outlook.com>
2017-03-09 21:12:03 -0300

    Implementação das ViewModels

---
+++ DDD/EstudoDDD.MVC/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstudoDDD.MVC.ViewModels
+{
+    public class ClienteViewModel
+    {
+        //ViewModels não devem ter comportamentos
+
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo nome")]
+        [MaxLength(150, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo sobrenome")]
+        [MaxLength(100, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo e-mail")]
+        [MaxLength(100, ErrorMessage = "Máximo {0} caracteres")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail válido")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]// O campo não é criado/exibido
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public IEnumerable<ProdutoViewModel> Produtos { get; set; }
+    }
+}
---
+++ DDD/EstudoDDD.MVC/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EstudoDDD.MVC.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo nome")]
+        [MaxLength(250, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo valor")]
+        [DataType(DataType.Currency)]
+        [Range(typeof(decimal), "0", "999999999999")]
+        public decimal Valor { get; set; }
+
+        [DisplayName("Disponível?")]
+        public bool Disponivel { get; set; }
+
+        public int ClienteId { get; set; }
+        public virtual ClienteViewModel Cliente { get; set; }
+    }
+}
