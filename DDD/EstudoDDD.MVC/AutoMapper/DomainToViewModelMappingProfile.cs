2da6a31ab0aa28580ec164a33c2c841dff230a14
Allan Barbosa <allan.hb@outlook.com>
2017-03-09 21:32:43 -0300

    Configuração do AutoMapper

---
+++ DDD/EstudoDDD.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using EstudoDDD.Domain.Entities;
+using EstudoDDD.MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudoDDD.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get
+            {
+                return "ViewModelToDomainMappingProfile";
+            }
+        }
+
+        protected override void Configure()
+        {
+            CreateMap<ClienteViewModel, Cliente>();
+            CreateMap<ProdutoViewModel, Produto>();
+        }
+    }
+}
---
+++ DDD/EstudoDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using EstudoDDD.Domain.Entities;
+using EstudoDDD.MVC.ViewModels;
+
+namespace EstudoDDD.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get
+            {
+                return "DomainToViewModelMappings";
+            }
+        }
+
+        protected override void Configure()
+        {
+            CreateMap<Cliente, ClienteViewModel>();
+            CreateMap<Produto, ProdutoViewModel>();
+        }
+    }
+}
