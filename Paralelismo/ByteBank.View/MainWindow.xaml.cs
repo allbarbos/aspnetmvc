0e319e623f08412eebf48387a11008ab6a9d10fb
Allan Barbosa <allan.hb@outlook.com>
2017-11-24 23:39:26 -0200

    Aula 02 - Task Factory / Waitall

--- Paralelismo/ByteBank.View/MainWindow.xaml.cs
+++ Paralelismo/ByteBank.View/MainWindow.xaml.cs
@@ -19,93 +19,52 @@ using System.Windows.Shapes;
 
 namespace ByteBank.View
 {
-    public partial class MainWindow : Window
+  public partial class MainWindow : Window
+  {
+    private readonly ContaClienteRepository r_Repositorio;
+    private readonly ContaClienteService r_Servico;
+
+    public MainWindow()
     {
-        private readonly ContaClienteRepository r_Repositorio;
-        private readonly ContaClienteService r_Servico;
+      InitializeComponent();
 
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            r_Repositorio = new ContaClienteRepository();
-            r_Servico = new ContaClienteService();
-        }
-
-        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
-        {
-            var contas = r_Repositorio.GetContaClientes();
-
-            var contasQuantidadePorThread = contas.Count() / 4;
-
-            var contas_parte1 = contas.Take(contasQuantidadePorThread);
-            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
-            var contas_parte3 = contas.Skip(contasQuantidadePorThread*2).Take(contasQuantidadePorThread);
-            var contas_parte4 = contas.Skip(contasQuantidadePorThread*3);
-
-            var resultado = new List<string>();
-
-            AtualizarView(new List<string>(), TimeSpan.Zero);
-
-            var inicio = DateTime.Now;
-
-            Thread thread_parte1 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte1)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte2 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte2)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte3 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte3)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte4 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte4)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-
-            thread_parte1.Start();
-            thread_parte2.Start();
-            thread_parte3.Start();
-            thread_parte4.Start();
-
-            while (thread_parte1.IsAlive || thread_parte2.IsAlive
-                || thread_parte3.IsAlive || thread_parte4.IsAlive )
-            {
-                Thread.Sleep(250);
-                //Não vou fazer nada
-            }
-            
-            var fim = DateTime.Now;
-
-            AtualizarView(resultado, fim - inicio);
-        }
-
-        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
-        {
-            var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
-            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
-
-            LstResultados.ItemsSource = result;
-            TxtTempo.Text = mensagem;
-        }
+      r_Repositorio = new ContaClienteRepository();
+      r_Servico = new ContaClienteService();
     }
+
+    private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+    {
+      var contas = r_Repositorio.GetContaClientes();
+
+      var resultado = new List<string>();
+
+      AtualizarView(new List<string>(), TimeSpan.Zero);
+
+      var inicio = DateTime.Now;
+
+      var contasTarefas = contas.Select(conta =>
+      {
+        return Task.Factory.StartNew(() =>
+        {
+          var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+          resultado.Add(resultadoConta);
+        });
+      }).ToArray();
+
+      Task.WaitAll(contasTarefas);
+
+      var fim = DateTime.Now;
+
+      AtualizarView(resultado, fim - inicio);
+    }
+
+    private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+    {
+      var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
+      var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+
+      LstResultados.ItemsSource = result;
+      TxtTempo.Text = mensagem;
+    }
+  }
 }
