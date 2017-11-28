06f0c6c09d7f2f0a5d2bad870c728d40d8fe6145
Allan Barbosa <allan.hb@outlook.com>
2017-11-27 23:40:04 -0200

    Aula 02 - TDD

---
+++ TDD/Leilao/Caelum.Leilao/Avaliador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Caelum.Leilao
+{
+  public class Avaliador
+  {
+    private double maiorDeTodos = Double.MinValue;
+    private double menorDeTodos = Double.MaxValue;
+    private double media = 0;
+    private List<Lance> maiores;
+
+    public void Avalia(Leilao leilao)
+    {
+      double total = 0;
+
+      foreach (Lance lance in leilao.Lances)
+      {
+        if (lance.Valor > maiorDeTodos)
+          maiorDeTodos = lance.Valor;
+        if (lance.Valor < menorDeTodos)
+          menorDeTodos = lance.Valor;
+        total += lance.Valor;
+      }
+
+      media = total / leilao.Lances.Count;
+
+      pegaOsMaioresNo(leilao);
+    }
+
+    public double MaiorLance { get { return maiorDeTodos; } }
+    public double MenorLance { get { return menorDeTodos; } }
+    public double Media { get { return media; } }
+
+    private void pegaOsMaioresNo(Leilao leilao)
+    {
+      maiores = new List<Lance>(leilao.Lances.OrderByDescending(x => x.Valor));
+      maiores = maiores.GetRange(0, 3);
+    }
+  }
+}
---
+++ TDD/Leilao/Caelum.Leilao/AvaliadorTest.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+
+namespace Caelum.Leilao
+{
+  [TestFixture]
+  public class AvaliadorTest
+  {
+    [Test]
+    public void DeveEntenderLancesEmOrdemCrescente()
+    {
+      // cenario: 3 lances em ordem crescente
+      Usuario joao = new Usuario("Joao");
+      Usuario jose = new Usuario("José");
+      Usuario maria = new Usuario("Maria");
+
+      Leilao leilao = new Leilao("Playstation 3 Novo");
+
+      leilao.Propoe(new Lance(maria, 250.0));
+      leilao.Propoe(new Lance(joao, 300.0));
+      leilao.Propoe(new Lance(jose, 400.0));
+
+      // executando a acao
+      Avaliador leiloeiro = new Avaliador();
+      leiloeiro.Avalia(leilao);
+
+      // comparando a saida com o esperado
+      Assert.AreEqual(400, leiloeiro.MaiorLance, 0.0001);
+      Assert.AreEqual(250, leiloeiro.MenorLance, 0.0001);
+    }
+
+    [Test]
+    public void DeveEntenderLancesEmOrdemCrescenteComOutrosValores()
+    {
+      Usuario joao = new Usuario("Joao");
+      Usuario jose = new Usuario("José");
+      Usuario maria = new Usuario("Maria");
+
+      Leilao leilao = new Leilao("Playstation 3 Novo");
+
+      leilao.Propoe(new Lance(maria, 1000.0));
+      leilao.Propoe(new Lance(joao, 2000.0));
+      leilao.Propoe(new Lance(jose, 3000.0));
+
+      Avaliador leiloeiro = new Avaliador();
+      leiloeiro.Avalia(leilao);
+
+      Assert.AreEqual(3000, leiloeiro.MaiorLance, 0.0001);
+      Assert.AreEqual(1000, leiloeiro.MenorLance, 0.0001);
+    }
+
+    [Test]
+    public void DeveEntenderLeilaoComApenasUmLance()
+    {
+      Usuario joao = new Usuario("Joao");
+      Leilao leilao = new Leilao("Playstation 3 Novo");
+
+      leilao.Propoe(new Lance(joao, 1000.0));
+
+      Avaliador leiloeiro = new Avaliador();
+      leiloeiro.Avalia(leilao);
+
+      Assert.AreEqual(1000, leiloeiro.MaiorLance, 0.0001);
+      Assert.AreEqual(1000, leiloeiro.MenorLance, 0.0001);
+    }
+
+    [Test]
+    public void DeveCalcularAMedia()
+    {
+      // cenario: 3 lances em ordem crescente
+      Usuario joao = new Usuario("Joao");
+      Usuario jose = new Usuario("José");
+      Usuario maria = new Usuario("Maria");
+
+      Leilao leilao = new Leilao("Playstation 3 Novo");
+
+      leilao.Propoe(new Lance(maria, 300.0));
+      leilao.Propoe(new Lance(joao, 400.0));
+      leilao.Propoe(new Lance(jose, 500.0));
+
+      // executando a acao
+      Avaliador leiloeiro = new Avaliador();
+      leiloeiro.Avalia(leilao);
+
+      // comparando a saida com o esperado
+      Assert.AreEqual(400, leiloeiro.Media, 0.0001);
+    }
+  }
+}
